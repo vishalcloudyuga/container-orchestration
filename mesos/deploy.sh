7f610d73181ea38645feea676920db1e716b780e
Neependra Khare <neependra.khare@gmail.com>
2016-09-24 19:47:05 +0530

    updated deploy.sh

--- mesos/deploy.sh
+++ mesos/deploy.sh
@@ -1,6 +1,7 @@
+curl -fsSL https://get.docker.com/ | sh
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 HOST_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo $HOST_IP
-export $HOST_IP
+export HOST_IP
 docker-compose up -d
