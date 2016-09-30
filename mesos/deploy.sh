28d7bc22ecacedfaef8800b58476a66d4da93b39
cloudyuga <neependra@cloudyuga.guru>
2016-09-30 10:11:58 +0000

    updates mesos

--- mesos/deploy.sh
+++ mesos/deploy.sh
@@ -3,5 +3,6 @@ curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 HOST_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo $HOST_IP
+sed -i "1s/^/nameserver $HOST_IP\n /" /etc/resolv.conf
 export HOST_IP
 docker-compose up -d
