82356f686213edb0f8c61b03e2129871bf6614c5
vishalcloudyuga <vishal@cloudyuga.guru>
2017-02-22 10:07:40 +0530

    Update deploy.sh

--- mesos-marathon/deploy.sh
+++ mesos-marathon/deploy.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod a+x /usr/local/bin/docker-compose
 HOST_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo $HOST_IP
